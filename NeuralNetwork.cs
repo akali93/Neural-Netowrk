4f8370783885e719b86bbd0d73f34cfadda14f59
Asaf Kali <akalk93@gmail.com>
2017-07-22 13:12:35 +0300

    First commit

---
+++ NeuralNetwork.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NN
+{
+    class NeuralNetwork
+    {
+        Random rnd;
+        private List<Transmitter> Inputs { get; set; }
+        private List<Receiver> Outputs { get; set; }
+        private List<List<Neuron>> Neurons { get; set; }
+        private int idCounter;
+        private readonly int reduction;
+
+        public NeuralNetwork()
+        {
+            idCounter = 1;
+            reduction = 4;
+            Neurons = new List<List<Neuron>>();
+            Inputs = new List<Transmitter>();
+            Outputs = new List<Receiver>();
+            rnd = new Random();
+        }
+
+        public void AddInput(Transmitter input)
+        {
+            input.ID = GetID();
+            Inputs.Add(input);
+        }
+
+        public void AddOutput(Receiver output)
+        {
+            output.ID = GetID();
+            Outputs.Add(output);
+        }
+
+        public void Build()
+        {
+            if (Inputs.Count < 1)
+                return;
+            if (Outputs.Count < 1)
+                return;
+            Neurons.Clear();
+            CreateLevel(Inputs, 0);
+        }
+
+        public void CreateLevel(List<Transmitter> inputs, int levelIndex)
+        {
+            bool lastRun = false; // Indicates if this is the last level building iteration
+            int actualReduction = this.reduction;
+            // Calculate number of neurons for this level
+            int neurCount = (int)Math.Ceiling((double)inputs.Count / actualReduction);
+            if (neurCount <= Outputs.Count)
+            {
+                // If neuron counter is less then output number, add neurons
+                while (neurCount < Outputs.Count)
+                    neurCount++;
+                actualReduction = (int)Math.Floor((double)inputs.Count / neurCount);
+                lastRun = true;
+            }
+            List<Neuron> level = new List<Neuron>();
+            int inputIndex = 0;
+            // For each neuron
+            for (int i = 0; i < neurCount; i++)
+            {
+                Neuron neuron = new Neuron(GetID());
+                // Connect inputs
+                for (int j = 0; (j < actualReduction) && (inputIndex < inputs.Count); j++)
+                    Connect(neuron, inputs[inputIndex++]);
+                level.Add(neuron);
+            }
+            // This will run on last level building. TODO: Check it again.
+            for (; inputIndex < inputs.Count;)
+                Connect(level[0], inputs[inputIndex++]);
+            // Save level
+            Neurons.Add(level);
+            if (lastRun)
+            {
+                // Connect last level neurons to outputs
+                for (int i = 0; i < level.Count; i++)
+                    level[i].AddOutput(Outputs[i]);
+                return;
+            }
+            // Create next level, when this level is the input
+            CreateLevel(level.ToList<Transmitter>(), levelIndex + 1);
+        } 
+
+        public void Connect(Neuron n, Transmitter input)
+        {
+            n.AddInput(input, rnd.NextDouble());
+            if (input is Neuron)
+                ((Neuron)input).AddOutput(n);
+        }
+
+        public void Activate()
+        {
+            if (Neurons != null)
+                if (Neurons.Count > 0)
+                    foreach (Neuron n in Neurons[0])
+                        n.AutoRecieve();
+                else
+                    throw new Exception("The Build function was not called");
+        }
+
+        public static double Normalize(double x)
+        {
+            return 1 / (1 + Math.Pow(Math.E, -x));
+        }
+
+        /*
+        public void Connect()
+        {
+            Neuron n1 = GetRandomNeuron();
+            Neuron n2 = GetRandomNeuron();
+            Connect(n1, n2);
+        }
+        
+        private Neuron GetRandomNeuron()
+        {
+            int index = rnd.Next(Neurons.Count);
+            return Neurons[index];
+        }*/
+
+        private int GetID()
+        {
+            return idCounter++;
+        }
+    }
+}
---
+++ Neuron.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NN
+{
+    class Neuron : Transmitter, Receiver
+    {
+        public int ID { get; set; }
+        public double Value { get; private set; }
+
+        private List<Transmitter> Inputs { get; set; }
+        private List<Receiver> Outputs { get; set; }
+        private Dictionary<int, double> Weights { get; set; }
+        private int RecieveCounter;
+
+        public Neuron(int id)
+        {
+            ID = id;
+            RecieveCounter = 0;
+            Inputs = new List<Transmitter>();
+            Outputs = new List<Receiver>();
+            Weights = new Dictionary<int, double>();
+        }
+
+        public bool AddInput(Transmitter t, double weight)
+        {
+            if (InputExists(t))
+                return false;
+            Inputs.Add(t);
+            Weights.Add(t.ID, weight);
+            return true;
+        }
+
+        public bool AddOutput(Receiver r)
+        {
+            if (OutputExists(r))
+                return false;
+            Outputs.Add(r);
+            return true;
+        }
+
+        public bool ChangeWeight(Transmitter t, double newWeight)
+        {
+            if (!InputExists(t))
+                return false;
+            throw new Exception("Look is this works in debug mode.");
+            Weights.Add(t.ID, newWeight);
+            return true;
+        }
+
+        public void AutoRecieve()
+        {
+            foreach (Transmitter t in Inputs)
+                Receive(t);
+        }
+
+        public void Receive(Transmitter t)
+        {
+            RecieveCounter++;
+            Value += t.Value * Weights[t.ID];
+            if (RecieveCounter >= Inputs.Count)
+                Transmit();
+        }
+
+        public void Transmit()
+        {
+            RecieveCounter = 0;
+            foreach (Receiver output in Outputs)
+                output.Receive(this);
+            this.Value = 0;
+        }
+
+        public bool InputExists(Transmitter t)
+        {
+            return Inputs.Contains(t);
+        }
+
+        public bool OutputExists(Receiver r)
+        {
+            return Outputs.Contains(r);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Identifiable)
+                return ID == ((Identifiable)obj).ID;
+            return false;
+        }
+
+        public override string ToString()
+        {
+            return "ID: " + ID + ", Value: " + Value;
+        }
+    }
+}
